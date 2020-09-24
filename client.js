055dfd17558d0253122360ab8ecbc8d36cab1233
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-24 04:39:17 +0000

    Refactor code and add sending name capabilityy

--- client.js
+++ client.js
@@ -8,9 +8,11 @@ const connect = function() {
     host: '135.23.222.131',
     port: 50542
   });
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  conn.write("Name: JLT");
   return conn;
 }
 
-module.exports = { connect };
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,11 +1,19 @@
 const { connect } = require('./client');
-console.log('Connecting ...');
+console.log('Attempting connection to game server.');
 const client = connect();
 
 client.on('connect', () => {
-  console.log('Successfully connected to server.');
+  console.log('Successfully connected to game server.');
 });
 
 client.on('data', (data) => {
-  console.log('Server says: ', data);
+  console.log('Game Server: ', data);
 });
+
+// stdin.on('data', (data) => {
+//   if (data === '\\q\n') {
+//     process.exit();
+//   }
+
+//   client.write(data);
+// })
